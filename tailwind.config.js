efd8e24221f5954c7c2a763987f6846aeb20c85b
Zakaria Elqarch <zakariaelqarch@gmail.com>
2024-08-25 15:19:16 +0100

    Project setup

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// tailwind.config.js
+
+module.exports = {
+  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      // Customizing theme settings if needed
+    },
+  },
+  plugins: [
+    // Add any Tailwind CSS plugins if needed
+  ],
+};
