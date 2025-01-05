e819e9ffd7de212e71675119fb8ecb04aec953d7
Zakaria Elqarch <zakariaelqarch@gmail.com>
2025-01-05 14:47:48 +0100

    update to expo 52 + create tab navigation + time: 2:10

--- app/(root)/_layout.tsx
+++ app/(root)/_layout.tsx
@@ -1,11 +1,22 @@
 import { Stack } from "expo-router";
-import { useEffect } from "react";
-import "react-native-reanimated";
 
 const Layout = () => {
   return (
     <Stack>
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="find-ride" options={{ headerShown: false }} />
+      <Stack.Screen
+        name="confirm-ride"
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="book-ride"
+        options={{
+          headerShown: false,
+        }}
+      />
     </Stack>
   );
 };
